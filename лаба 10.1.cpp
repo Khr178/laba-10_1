a979e10394bc86b0d2459303b14d7256c2f28beb
Khrystyna <hrustunakopeika2005@gmail.com>
2022-12-17 19:01:17 +0200

    first master commit

---
+++ лаба 10.1.cpp
@@ -0,0 +1,42 @@
+﻿// лаба 10.1.cpp 
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void PrintTXT(char* fname) 
+{
+	ifstream fin(fname); 
+	string s; 
+	while (getline(fin, s)) 
+	{
+		cout << s << endl; 
+	}
+	cout << endl;
+}
+int ProcessTXT1(char* fname) 
+{
+	ifstream fin(fname); 
+	string s; 
+	int k = 0; 
+	while (getline(fin, s)) 
+	{ 
+		for (unsigned i = 0; i < s.length(); i++)
+			if (s[i] == 'w' || s[i+1] == 'h' || s[i+2] == 'i' || s[i+3] == 'l' || s[i+4] == 'e')
+				k++;
+	}
+	return k;
+}
+int main()
+{
+	// text files
+	char fname[100]; 
+	cout << "enter file name 1: "; cin >> fname;
+
+	PrintTXT(fname); 
+	cout << "k(while) = " << ProcessTXT1(fname) << endl;
+
+	return 0;
+}
---
+++ юніт тест 10.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 10.1/лаба 10.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест101
+{
+	TEST_CLASS(юніттест101)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int k;
+			k = main();
+			Assert::AreEqual(k, 0);
+		}
+	};
+}
